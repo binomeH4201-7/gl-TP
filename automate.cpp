f03b511b38301a51d97922e2d5febad3bb029cb7
Phanie Buonomo <fanny.buonomo@insa-lyon.fr>
2021-02-26 09:53:12 +0100

    correction de la méthode reduction

--- automate.cpp
+++ automate.cpp
@@ -6,4 +6,13 @@ void Automate::decalage(Etat * etat) {
 
 void Automate::reduction(Symbole * sym, int aReduire) {
 
+  /*Etape 1 : on dépile le nombre de symbole
+    Etape 2 : on applique la transition depuis le NOUVEAU sommet de la pile avec le symbole sym
+   */
+
+  for(int i =0; i < aReduire; i++){
+    pile.pop_back();
+  }
+
+  pile.back()->transition(*this, sym);
 }
