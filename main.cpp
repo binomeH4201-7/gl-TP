4fbc9fcd7e14169582ffe03be30ba4e0863230a3
INSArthur <49277717+INSArthur@users.noreply.github.com>
2021-02-26 11:58:08 +0100

    déclaration automate

--- main.cpp
+++ main.cpp
@@ -1,25 +1,30 @@
 #include <iostream>
 #include "lexer.h"
+#include "automate.h"
 
 
 int main(void) {
    string chaine("(1+34)*123");
 
-   Lexer l(chaine);
-
+    Automate* automate = new Automate(chaine);
+    
    /*Creer l'automate, init la pile à l'état 0
     Lire le premier symbole
     Empiler le symbole
     Faire la transition (etat.transition qui appelle automate.reduction ou automate.decalage)
     Si on a un decalage, on deplace la tete de lecture (pas dans le cas d'une reduction
     */
-    
-   Symbole * s;
+
+    /*
+    Lexer l(chaine);
+
+    Symbole * s;
    while(*(s=l.Consulter())!=FIN) { 
       s->Affiche();
       cout<<endl;
       l.Avancer();
    }
+     */
    return 0;
 }
 
