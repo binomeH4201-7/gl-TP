a8e3fd8ba309aa1afe4db92ba31332f55e16bb18
Phanie Buonomo <fanny.buonomo@insa-lyon.fr>
2021-02-26 11:20:39 +0100

    ajout d'une pile de symbole

--- automate.cpp
+++ automate.cpp
@@ -1,18 +1,23 @@
 #include "automate.h"
 
 void Automate::decalage(Etat * etat) {
-  pile.push_back(etat);
+  pileEtat.push_back(etat);
 }
 
 void Automate::reduction(Symbole * sym, int aReduire) {
 
   /*Etape 1 : on dépile le nombre de symbole
-    Etape 2 : on applique la transition depuis le NOUVEAU sommet de la pile avec le symbole sym
+    Etape 2 : on depile le nombre d'état
+    Etape 3 : on empile la partie gauche de la règle
+    Etape 4 : on applique la transition depuis le NOUVEAU sommet de la pile avec le sommet de la pile de symbole
    */
 
   for(int i =0; i < aReduire; i++){
-    pile.pop_back();
+    pileEtat.pop_back();
+    pileSymbole.pop_back();
   }
 
-  pile.back()->transition(*this, sym);
+  pileSymbole.push_back(sym);
+
+  pileEtat.back()->transition(*this, pileSymbole.back());
 }
