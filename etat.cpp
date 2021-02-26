2a80fb5e43b88d3cb23c418d61be3bf88722e335
INSArthur <49277717+INSArthur@users.noreply.github.com>
2021-02-26 10:49:42 +0100

    transition

--- etat.cpp
+++ etat.cpp
@@ -2,4 +2,217 @@
 // Created by Arthur Batel on 26/02/2021.
 //
 
+#include <map>
 #include "etat.h"
+#include "iostream"
+using namespace std;
+
+std::map<int,int> regleToNbMembres = {{1,1},{2,3},{3,3},{4,3},{5,1}};
+
+Etat::~Etat(){}
+void Etat::print() const{
+    cout << name << endl;
+}
+
+
+bool E0::transition(Automate &automate, Symbole *s) {
+    switch (*s){
+        case OPENPAR:
+            automate.decalage(new E2());
+            break;
+        case INT:
+            automate.decalage(new E3());
+            break;
+        case ERREUR:
+            cout << "Erreur" <<endl;
+            break;
+        default :
+            break;
+    }
+    return false;
+}
+
+bool E1::transition(Automate &automate, Symbole *s) {
+    switch (*s){
+        case PLUS:
+            automate.decalage(new E4());
+            break;
+        case MULT:
+            automate.decalage(new E5());
+            break;
+        case FIN:
+            //TODO: Implémenter la fin
+            cout << "FIN" <<endl;
+            break;
+        case ERREUR:
+            cout << "Erreur" <<endl;
+            break;
+        default :
+            break;
+    }
+    return false;
+}
+
+bool E2::transition(Automate &automate, Symbole *s) {
+    switch (*s){
+        case INT:
+            automate.decalage(new E3());
+            break;
+        case OPENPAR:
+            automate.decalage(new E2());
+            break;
+        case ERREUR:
+            cout << "Erreur" <<endl;
+            break;
+        default :
+            break;
+    }
+    return false;
+}
+
+bool E3::transition(Automate &automate, Symbole *s) {
+    switch (*s){
+        case PLUS:
+            automate.reduction(s,regleToNbMembres[5]);
+            break;
+        case MULT:
+            automate.reduction(s,regleToNbMembres[5]);
+            break;
+        case OPENPAR:
+            automate.reduction(s,regleToNbMembres[5]);
+            break;
+        case FIN:
+            automate.reduction(s,regleToNbMembres[5]);
+            break;
+        case ERREUR:
+            cout << "Erreur" <<endl;
+            break;
+        default :
+            break;
+    }
+    return false;
+}
+
+bool E4::transition(Automate &automate, Symbole *s) {
+    switch (*s){
+        case INT:
+            automate.decalage(new E3());
+            break;
+        case OPENPAR:
+            automate.decalage(new E2());
+            break;
+        case ERREUR:
+            cout << "Erreur" <<endl;
+            break;
+        default :
+            break;
+    }
+    return false;
+}
+
+bool E5::transition(Automate &automate, Symbole *s) {
+    switch (*s){
+        case INT:
+            automate.decalage(new E3());
+            break;
+        case OPENPAR:
+            automate.decalage(new E2());
+            break;
+        case ERREUR:
+            cout << "Erreur" <<endl;
+            break;
+        default :
+            break;
+    }
+    return false;
+}
+
+bool E6::transition(Automate &automate, Symbole *s) {
+    switch (*s){
+        case PLUS:
+            automate.decalage(new E4());
+            break;
+        case MULT:
+            automate.decalage(new E5());
+            break;
+        case CLOSEPAR:
+            automate.decalage(new E9());
+            break;
+        case ERREUR:
+            cout << "Erreur" <<endl;
+            break;
+        default :
+            break;
+    }
+    return false;
+}
+
+
+bool E7::transition(Automate &automate, Symbole *s) {
+    switch (*s){
+        case PLUS:
+            automate.reduction(s, regleToNbMembres[2]);
+            break;
+        case MULT:
+            automate.decalage(new E5());
+            break;
+        case CLOSEPAR:
+            automate.reduction(s,regleToNbMembres[2]);
+            break;
+        case FIN:
+            automate.reduction(s, regleToNbMembres[2]);
+            break;
+        case ERREUR:
+            cout << "Erreur" <<endl;
+            break;
+        default :
+            break;
+    }
+    return false;
+}
+
+bool E8::transition(Automate &automate, Symbole *s) {
+    switch (*s){
+        case PLUS:
+            automate.reduction(s, regleToNbMembres[3]);
+            break;
+        case MULT:
+            automate.reduction(s, regleToNbMembres[3]);
+            break;
+        case CLOSEPAR:
+            automate.reduction(s, regleToNbMembres[3]);
+            break;
+        case FIN:
+            automate.reduction(s, regleToNbMembres[3]);
+            break;
+        case ERREUR:
+            cout << "Erreur" <<endl;
+            break;
+        default :
+            break;
+    }
+    return false;
+}
+
+bool E9::transition(Automate &automate, Symbole *s) {
+    switch (*s){
+        case PLUS:
+            automate.reduction(s, regleToNbMembres[4]);
+            break;
+        case MULT:
+            automate.reduction(s, regleToNbMembres[4]);
+            break;
+        case CLOSEPAR:
+            automate.reduction(s, regleToNbMembres[4]);
+            break;
+        case FIN:
+            automate.reduction(s, regleToNbMembres[4]);
+            break;
+        case ERREUR:
+            cout << "Erreur" <<endl;
+            break;
+        default :
+            break;
+    }
+    return false;
+}
--- etat.h
+++ etat.h
@@ -27,62 +27,78 @@ protected:
 
 class E0 : public Etat{
 public :
-    E0(string aName) : Etat(aName){}
+    E0() : Etat("E0"){}
     virtual ~E0();
+    bool transition(Automate &automate, Symbole *s);
 };
 
 class E1 : public Etat{
 public :
-    E1(string aName) : Etat(aName){}
+    E1() : Etat("E1"){}
     virtual ~E1();
+    bool transition(Automate &automate, Symbole *s);
 };
 
 class E2 : public Etat{
 public :
-    E2(string aName) : Etat(aName){}
+    E2() : Etat("E2"){}
     virtual ~E2();
+    bool transition(Automate &automate, Symbole *s);
 };
 
 class E3 : public Etat{
 public :
-    E3(string aName) : Etat(aName){}
+    E3() : Etat("E3"){}
     virtual ~E3();
+    bool transition(Automate &automate, Symbole *s);
+
 };
 
 class E4 : public Etat{
 public :
-    E4(string aName) : Etat(aName){}
+    E4() : Etat("E4"){}
     virtual ~E4();
+    bool transition(Automate &automate, Symbole *s);
 };
 
 class E5 : public Etat{
 public :
-    E5(string aName) : Etat(aName){}
+    E5() : Etat("E5"){}
     virtual ~E5();
+    bool transition(Automate &automate, Symbole *s);
+
 };
 
 class E6 : public Etat{
 public :
-    E6(string aName) : Etat(aName){}
+    E6() : Etat("E6"){}
     virtual ~E6();
+    bool transition(Automate &automate, Symbole *s);
+
 };
 
 class E7 : public Etat{
 public :
-    E7(string aName) : Etat(aName){}
+    E7() : Etat("E7"){}
     virtual ~E7();
+    bool transition(Automate &automate, Symbole *s);
+
 };
 
 class E8 : public Etat{
 public :
-    E8(string aName) : Etat(aName){}
+    E8() : Etat("E8"){}
     virtual ~E8();
+    bool transition(Automate &automate, Symbole *s);
+
 };
 
 class E9 : public Etat{
 public :
-    E9(string aName) : Etat(aName){}
+    E9() : Etat("E9"){}
     virtual ~E9();
+    bool transition(Automate &automate, Symbole *s);
+
 };
 
 
