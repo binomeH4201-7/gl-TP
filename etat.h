90e8d4d29e42d6d991f31c35ca24676b633a49dc
INSArthur <49277717+INSArthur@users.noreply.github.com>
2021-02-26 09:30:48 +0100

    string constructeur

--- etat.h
+++ etat.h
@@ -26,61 +26,61 @@ protected:
 
 class E0 : public Etat{
 public :
-    E0();
+    E0(string name);
     ~E0();
 };
 
 class E1 : public Etat{
 public :
-    E1();
+    E1(string name);
     ~E1();
 };
 
 class E2 : public Etat{
 public :
-    E2();
+    E2(string name);
     ~E2();
 };
 
 class E3 : public Etat{
 public :
-    E3();
+    E3(string name);
     ~E3();
 };
 
 class E4 : public Etat{
 public :
-    E4();
+    E4(string name);
     ~E4();
 };
 
 class E5 : public Etat{
 public :
-    E5();
+    E5(string name);
     ~E5();
 };
 
 class E6 : public Etat{
 public :
-    E6();
+    E6(string name);
     ~E6();
 };
 
 class E7 : public Etat{
 public :
-    E7
+    E7(string name);
     ~E7();
 };
 
 class E8 : public Etat{
 public :
-    E8();
+    E8(string name);
     ~E8();
 };
 
 class E9 : public Etat{
 public :
-    E9();
+    E9(string name);
     ~E9();
 };
 
