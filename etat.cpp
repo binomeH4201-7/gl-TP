f5c8a771daa3ea3eac01a7042943d9143ea2bbe5
INSArthur <49277717+INSArthur@users.noreply.github.com>
2021-02-26 11:19:40 +0100

    v table

--- etat.cpp
+++ etat.cpp
@@ -9,7 +9,6 @@ using namespace std;
 
 std::map<int,int> regleToNbMembres = {{1,1},{2,3},{3,3},{4,3},{5,1}};
 
-Etat::~Etat(){}
 void Etat::print() const{
     cout << name << endl;
 }
--- etat.h
+++ etat.h
@@ -17,7 +17,7 @@ class Automate;
 class Etat {
 public :
     Etat(string aName) : name(aName){}
-    virtual ~Etat();
+    virtual ~Etat(){}
     void print() const;
     virtual bool transition(Automate & automate, Symbole * s) = 0;
 
@@ -28,28 +28,28 @@ protected:
 class E0 : public Etat{
 public :
     E0() : Etat("E0"){}
-    virtual ~E0();
+    virtual ~E0(){}
     bool transition(Automate &automate, Symbole *s);
 };
 
 class E1 : public Etat{
 public :
     E1() : Etat("E1"){}
-    virtual ~E1();
+    virtual ~E1(){}
     bool transition(Automate &automate, Symbole *s);
 };
 
 class E2 : public Etat{
 public :
     E2() : Etat("E2"){}
-    virtual ~E2();
+    virtual ~E2(){}
     bool transition(Automate &automate, Symbole *s);
 };
 
 class E3 : public Etat{
 public :
     E3() : Etat("E3"){}
-    virtual ~E3();
+    virtual ~E3(){}
     bool transition(Automate &automate, Symbole *s);
 
 };
@@ -57,14 +57,14 @@ public :
 class E4 : public Etat{
 public :
     E4() : Etat("E4"){}
-    virtual ~E4();
+    virtual ~E4(){}
     bool transition(Automate &automate, Symbole *s);
 };
 
 class E5 : public Etat{
 public :
     E5() : Etat("E5"){}
-    virtual ~E5();
+    virtual ~E5(){}
     bool transition(Automate &automate, Symbole *s);
 
 };
@@ -72,7 +72,7 @@ public :
 class E6 : public Etat{
 public :
     E6() : Etat("E6"){}
-    virtual ~E6();
+    virtual ~E6(){}
     bool transition(Automate &automate, Symbole *s);
 
 };
@@ -80,7 +80,7 @@ public :
 class E7 : public Etat{
 public :
     E7() : Etat("E7"){}
-    virtual ~E7();
+    virtual ~E7(){}
     bool transition(Automate &automate, Symbole *s);
 
 };
@@ -88,7 +88,7 @@ public :
 class E8 : public Etat{
 public :
     E8() : Etat("E8"){}
-    virtual ~E8();
+    virtual ~E8(){}
     bool transition(Automate &automate, Symbole *s);
 
 };
@@ -96,7 +96,7 @@ public :
 class E9 : public Etat{
 public :
     E9() : Etat("E9"){}
-    virtual ~E9();
+    virtual ~E9(){}
     bool transition(Automate &automate, Symbole *s);
 
 };
